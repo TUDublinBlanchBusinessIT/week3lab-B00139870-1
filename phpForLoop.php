664ff101b412f3754965f70008b027ae969da075
B00139870 Komal Wajahat <B00139870@mytudublin.ie>
2023-10-04 10:07:13 +0100

    changed it to 20

--- phpForLoop.php
+++ phpForLoop.php
@@ -3,8 +3,8 @@
 #put your name and the date here
 #put a descripition of what the code does here - 
 #this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
-for ($i=1;$i<=10;$i++) {
-    echo "$i, ";
+for ($i=0;$i<=100;$i++) {
+    echo "$i, x";
 }
 
 ?>
